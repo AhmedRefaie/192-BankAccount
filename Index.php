35eb09b01ef2d169eafebf0e8786b7f9e1ee2e5c
AhmedRefaie <31232211+AhmedRefaie@users.noreply.github.com>
2018-02-02 01:43:41 +0200

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,49 @@
+<?php
+
+class BankAccount {
+	public $balance = 100000000;
+	
+	public function DisplayBalance(){
+		return 'Balance: '.$this->balance;
+	}
+	
+	public function Withdraw($amount){
+		if (($this->balance)<$amount){
+			echo 'Not enough money.'.'<br>';
+		} else {
+		$this->balance = $this->balance - $amount;
+		}
+	}
+	
+}
+
+// new instance of class
+$ref = new BankAccount ;
+
+// withdrawing 5
+$ref->Withdraw(600000000000000);
+
+// displaying balance
+echo $ref->DisplayBalance() ;
+
+?>  
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+//OOP
+
+// Object Orianted Programing 
+
+?>  
+
+
